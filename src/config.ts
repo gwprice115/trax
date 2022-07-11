c6345a9df43b38484362f7e84d1f941059cdb163
George Price <gprice@palantir.com>
2022-07-11 17:19:28 -0400

    approximate beginning

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,18 @@ export default {
   type: Phaser.AUTO,
   parent: 'game',
   backgroundColor: '#33A5E7',
+
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
   scale: {
     width: 800,
     height: 600,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   }
+
 };
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,129 @@
 import Phaser from 'phaser';
 
+
+
 export default class Demo extends Phaser.Scene {
   constructor() {
     super('GameScene');
   }
 
+  private score = 0;
+
+  private hitBomb = (player: Phaser.Types.Physics.Arcade.GameObjectWithBody, bomb: Phaser.Types.Physics.Arcade.GameObjectWithBody) => {
+    this.physics.pause();
+
+    (player as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).setTint(0xff0000);
+
+    (player as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).anims.play('turn');
+  }
+
+
+  private collectStar = (player: Phaser.Types.Physics.Arcade.GameObjectWithBody, star: Phaser.Types.Physics.Arcade.GameObjectWithBody) => {
+
+    (star as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).disableBody(true, true);
+    this.score += 10;
+    this.registry.get('scoreText').setText('Score: ' + this.score);
+    var x = ((player as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+
+    const bomb = this.registry.get('bombs').create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+
+  }
+
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.setBaseURL('http://labs.phaser.io');
+    this.load.image('sky', 'assets/skies/sky4.png');
+    this.load.image('ground', 'assets/sprites/platform.png');
+    this.load.image('star', 'assets/demoscene/star.png');
+    this.load.image('bomb', 'assets/sprites/apple.png');
+    this.load.spritesheet('dude',
+      'assets/sprites/dude.png',
+      { frameWidth: 32, frameHeight: 48 }
+    );
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
+    this.add.image(400, 300, 'sky');
+    const player = this.physics.add.sprite(100, 450, 'dude');
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
+    player.setCollideWorldBounds(true);
+    this.registry.set('player', player);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
       repeat: -1
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'dude', frame: 4 }],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    const cursors = this.input.keyboard.createCursorKeys();
+    this.registry.set('cursors', cursors);
+
+
+    const stars = this.physics.add.group({
+      key: 'star',
+      repeat: 110,
+      setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+      const typedChild = child as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+      typedChild.setY(Phaser.Math.FloatBetween(0, 500));
+    });
+
+    this.physics.add.overlap(player, stars, this.collectStar, undefined, this);
+
+
+    const bombs = this.physics.add.group();
+    this.registry.set('bombs', bombs);
+    this.physics.add.collider(player, bombs, this.hitBomb, undefined, this);
+
+    const scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', color: '#000' });
+    this.registry.set('scoreText', scoreText);
+
+    const speedables = [bombs,stars].filter(a => a != null);
+    speedables.forEach((childGroup) => {
+      childGroup.children.iterate(child =>
+        (child as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).setVelocityX(-100))
+    });
+  }
+  update() {
+    const cursors = this.registry.get('cursors');
+    const player = this.registry.get('player');
+    if (cursors.up.isDown) {
+      player.setVelocityY(-160);
+
+      player.anims.play('left', true);
+    }
+    else if (cursors.down.isDown) {
+      player.setVelocityY(160);
+
+      player.anims.play('right', true);
+    }
+    else {
+      player.setVelocityY(0);
+
+      player.anims.play('turn');
+    }
+    const speedables = [this.registry.get('bombs'), this.registry.get('stars')].filter(a => a != null);
+    speedables.forEach((childGroup) => {
+      childGroup.children.iterate(child =>
+        (child as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).setVelocityX(-100))
     });
   }
 }
