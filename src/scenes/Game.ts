80a62cc628e46ca4ce7a3b8c7dfcf0a8eab7d4f7
Jessica Cheng <jessicacheng@palantir.com>
2022-07-12 13:30:03 -0400

    costume change

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,13 +1,12 @@
 import Phaser from 'phaser';
 
-
-
 export default class Demo extends Phaser.Scene {
   constructor() {
     super('GameScene');
   }
 
   private score = 0;
+  private costume = 'dude';
 
   private hitBomb = (player: Phaser.Types.Physics.Arcade.GameObjectWithBody, bomb: Phaser.Types.Physics.Arcade.GameObjectWithBody) => {
     this.physics.pause();
@@ -17,7 +16,6 @@ export default class Demo extends Phaser.Scene {
     (player as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).anims.play('turn');
   }
 
-
   private collectStar = (player: Phaser.Types.Physics.Arcade.GameObjectWithBody, star: Phaser.Types.Physics.Arcade.GameObjectWithBody) => {
     (star as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).disableBody(true, true);
     this.score += 10;
@@ -26,25 +24,42 @@ export default class Demo extends Phaser.Scene {
     this.registry.get('bombs').create(800, y, 'bomb');
   }
 
+  private changeCostume = (player: Phaser.Types.Physics.Arcade.GameObjectWithBody, portal: Phaser.Types.Physics.Arcade.GameObjectWithBody) => {
+    (portal as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).disableBody(true, true);
+    console.log(this.costume)
+    switch(this.costume) {
+      case 'dude':
+        this.costume = 'chick';
+    }
+    console.log(this.costume)
+  }
+
   preload() {
     this.load.setBaseURL('http://labs.phaser.io');
     this.load.image('sky', 'assets/skies/sky4.png');
     this.load.image('ground', 'assets/sprites/platform.png');
     this.load.image('star', 'assets/demoscene/star.png');
     this.load.image('bomb', 'assets/sprites/apple.png');
+    this.load.image('portal', 'assets/sprites/mushroom.png')
     this.load.spritesheet('dude',
       'assets/sprites/dude.png',
+      { frameWidth: 12, frameHeight: 12 }
+    );
+    this.load.spritesheet('chick',
+      'assets/sprites/chick.png',
       { frameWidth: 32, frameHeight: 48 }
     );
   }
 
   create() {
     this.add.image(400, 300, 'sky');
-    const player = this.physics.add.sprite(100, 450, 'dude');
 
+    // load dude in
+    const player = this.physics.add.sprite(100, 450, 'dude');
     player.setCollideWorldBounds(true);
     this.registry.set('player', player);
 
+    // define animations for dude
     this.anims.create({
       key: 'left',
       frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
@@ -65,10 +80,11 @@ export default class Demo extends Phaser.Scene {
       repeat: -1
     });
 
+    // load key inputs
     const cursors = this.input.keyboard.createCursorKeys();
     this.registry.set('cursors', cursors);
 
-
+    // load stars and set collider
     const stars = this.physics.add.group({
       key: 'star',
       repeat: 110,
@@ -81,40 +97,60 @@ export default class Demo extends Phaser.Scene {
     });
 
     this.physics.add.overlap(player, stars, this.collectStar, undefined, this);
+    this.registry.set('stars', stars);
 
-
+    // load bombs and set collider
     const bombs = this.physics.add.group();
     this.registry.set('bombs', bombs);
     this.physics.add.collider(player, bombs, this.hitBomb, undefined, this);
 
+    // load portals and set collider
+    const portals = this.physics.add.group({
+      key: 'portal',
+      repeat: 500,
+      setXY: { x: 12, y: 400, stepX: 700 }
+    });
+
+    portals.children.iterate(function (child) {
+      const typedChild = child as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+      typedChild.setY(Phaser.Math.FloatBetween(0, 500));
+    });
+
+    this.physics.add.overlap(player, portals, this.changeCostume, undefined, this);
+
+    // load score text
     const scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', color: '#000' });
     this.registry.set('scoreText', scoreText);
 
-    this.registry.set('speedables', [bombs, stars]);
+    this.registry.set('speedables', [bombs, stars, portals]);
   }
   update() {
+    // update dude position
     const cursors = this.registry.get('cursors');
     const player = this.registry.get('player');
     if (cursors.up.isDown) {
       player.setVelocityY(-160);
-
       player.anims.play('left', true);
     }
     else if (cursors.down.isDown) {
       player.setVelocityY(160);
-
       player.anims.play('right', true);
     }
     else {
       player.setVelocityY(0);
-
       player.anims.play('turn');
     }
 
+    // update obstacle position to scroll left
     const speedables: Phaser.Physics.Arcade.Group[] = this.registry.get('speedables');
     speedables.forEach((childGroup) => {
       childGroup.children.iterate(child =>
         (child as Phaser.Types.Physics.Arcade.SpriteWithDynamicBody).setVelocityX(-100))
     });
+
+    // update dude costume
+    if (this.costume === 'chick') {
+      player.setTexture('chick')
+    }
   }
 }
