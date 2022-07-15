d3f630f1b366c5c3086e236b4a4921dbb157afeb
Jessica Cheng <jessicacheng@palantir.com>
2022-07-15 14:17:32 -0400

    clean up files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import Phaser from 'phaser';
 import { config } from './config';
 import GameScene from './scenes/Game';
-import Highscore from './scenes/Highscore';
 
 new Phaser.Game(
   Object.assign(config, {
-    scene: [GameScene, Highscore]
+    scene: [GameScene]
   })
 );
 
